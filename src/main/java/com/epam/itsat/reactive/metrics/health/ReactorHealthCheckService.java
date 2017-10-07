c775b758b6fa98bd9dd2d9768c5181a43a2cc611
Andrey Volodin <lctncld@gmail.com>
2017-10-07 18:22:06 +0300

    Yay!

--- src/main/java/com/epam/itsat/reactive/metrics/health/ReactorHealthCheckService.java
+++ src/main/java/com/epam/itsat/reactive/metrics/health/ReactorHealthCheckService.java
@@ -4,6 +4,7 @@ import com.epam.itsat.reactive.metrics.health.counter.RequestCounterService;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Service
@@ -19,12 +20,14 @@ class ReactorHealthCheckService implements HealthCheckService {
 	public Flux<RequestMetrics> getRequestMetrics() {
 		Flux<Long> allRequests = Flux.generate(sink -> sink.next(counter.getTotalRequestCount()));
 		Flux<Long> failedRequests = Flux.generate(sink -> sink.next(counter.getFailedRequestCount()));
-		return Flux.zip(allRequests, failedRequests)
-				.map(tuple -> RequestMetrics.builder()
-						.total(tuple.getT1())
-						.failed(tuple.getT2())
-						.timestamp(LocalDateTime.now())
-						.build()
-				);
+		return Flux.zip(
+				allRequests.delayElements(Duration.ofSeconds(1)),
+				failedRequests.delayElements(Duration.ofSeconds(1))
+		).map(tuple -> RequestMetrics.builder()
+				.total(tuple.getT1())
+				.failed(tuple.getT2())
+				.timestamp(LocalDateTime.now())
+				.build()
+		);
 	}
 }
